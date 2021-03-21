0763c7b6cf498f7d92ecf3821517129a531b538c
VickyDhanwani <vickydhanani11@gmail.com>
2021-03-21 22:25:21 +0530

    tax functions working. Frontend integrated.

--- frontend/index.js
+++ frontend/index.js
@@ -9,13 +9,17 @@ var old_tax_slabs = [0, 5, 20, 30]
 var new_tax_regime_upper_limit = [249999, 499999, 749999, 999999, 1249999, 100000000000000];
 var new_tax_regime_lower_limit = [0, 250000, 500000, 750000, 1000000, 1250000];
 var new_tax_regime_slabs = ["0 - 2.5L", "2.5L - 5.0L", "5.0L - 7.5L", "7.5L - 10.0L", "10.0L - 12.5L", "Above 12.5L"];
-var new_tax_slabs = [0, 5, 10, 20, 25, 30]
+var new_tax_slabs = [0, 5, 10, 20, 25, 30];
 var i, j, k;
 var t_income;
 var slab_index;
 var tax_calculated;
 var total_deductions = parseInt(document.getElementsByClassName("deductions-value")[0].value);
-console.log(total_deductions, income);
+var old_regime_tax = 0;
+var new_regime_tax = 0;
+var old_regime_slab = 0;
+var new_regime_slab = 0;
+
 CalculateTaxFunction = (slab, income, UpperLimits, LowerLimits, SlabValues) => {
     var i = 0;
     var CalculatedTax = 0;
@@ -44,29 +48,49 @@ CalculateTaxSlabIndex = (income, UpperLimits) => {
     return calculatedSlab;
 }
 
-if(tax_regime === options[0]) {
 
-	total_deductions += standard_deduction
-	console.log("Your total deduction is ", total_deductions);
-	t_income = income - total_deductions;
-	tax_calculated = 0;
-	slab_index = 0;
-	console.log("Calculating...");
-	slab_index = CalculateTaxSlabIndex(t_income, old_tax_regime_upper_limit);
-	console.log("You fit under ",old_tax_regime_slabs[slab_index], " tax slab.");
-	tax_calculated = CalculateTaxFunction(slab_index, t_income, old_tax_regime_upper_limit, old_tax_regime_lower_limit, old_tax_slabs);
-	console.log("Total calculated tax under new regime is : ", tax_calculated);
+CalculateTax = () => {
+	var output_string = "";
+	this.income = parseInt(document.getElementsByClassName("income-value")[0].value);
+	this.total_deductions = parseInt(document.getElementsByClassName("deductions-value")[0].value);
+	//console.log(total_deductions, income);
+	if(tax_regime === options[0]) {
+		total_deductions += standard_deduction
+		//console.log("Your total deduction is ", total_deductions);
+		t_income = income - total_deductions;
+		tax_calculated = 0;
+		slab_index = 0;
+		//console.log("Calculating...");
+		slab_index = CalculateTaxSlabIndex(t_income, old_tax_regime_upper_limit);
+		//console.log("You fit under ",old_tax_regime_slabs[slab_index], " tax slab.");
+		tax_calculated = CalculateTaxFunction(slab_index, t_income, old_tax_regime_upper_limit, old_tax_regime_lower_limit, old_tax_slabs);
+		//console.log("Total calculated tax under new regime is : ", tax_calculated);
+		this.old_regime_tax = this.tax_calculated;
+		this.old_regime_slab = slab_index;
+		output_string = "For Old Tax Regime <br>";
+		output_string += "Your Taxable Income is " + t_income + "<br>"
+		output_string += "You fit under " + old_tax_regime_slabs[slab_index] + " tax slab. <br>";
+		output_string += "Tax Calculated is <b>" + this.old_regime_tax + "</b>."
+		document.getElementsByClassName("old-regime-output")[0].innerHTML = output_string;
+	}
+	this.tax_calculated = 0;
+	tax_regime = options[1];
+	if(tax_regime === options[1]) {
+		i = 0;
+		t_income = income;
+		tax_calculated = 0;
+		slab_index = 0;
+		//console.log("Calculating...");
+		slab_index = CalculateTaxSlabIndex(t_income, new_tax_regime_upper_limit);
+		//console.log("You fit under ",new_tax_regime_slabs[slab_index], " tax slab.");
+		tax_calculated = CalculateTaxFunction(slab_index, t_income, new_tax_regime_upper_limit, new_tax_regime_lower_limit, new_tax_slabs);
+		//console.log("Total calculated tax under new regime is : ", tax_calculated);
+		this.new_regime_tax = this.tax_calculated;
+		this.new_regime_slab = slab_index;
+		output_string = "For New Tax Regime <br>";
+		output_string += "Your Taxable Income is " + t_income + "<br>"
+		output_string += "You fit under " + new_tax_regime_slabs[slab_index] + " tax slab. <br>";
+		output_string += "Tax Calculated is <b>" + this.new_regime_tax + "</b>."
+		document.getElementsByClassName("new-regime-ouput")[0].innerHTML = output_string;
+	}	
 }
-else if(tax_regime === options[1]) {
-	i = 0;
-	t_income = income;
-	tax_calculated = 0;
-	slab_index = 0;
-	console.log("Calculating...");
-	slab_index = CalculateTaxSlabIndex(t_income, new_tax_regime_upper_limit);
-	console.log("You fit under ",new_tax_regime_slabs[slab_index], " tax slab.");
-	tax_calculated = CalculateTaxFunction(slab_index, t_income, new_tax_regime_upper_limit, new_tax_regime_lower_limit, new_tax_slabs);
-	console.log("Total calculated tax under new regime is : ", tax_calculated);
-}
-
-document.getElementsByClassName("output-text").innerHTML = "dsjfndsf";
